0e3e29cd75feb99092401137d35de5d82d333f8e
RYO <ostantares@gmail.com>
2018-05-08 17:49:12 +0800

    Update main.js

--- main.js
+++ main.js
@@ -18,23 +18,31 @@ function hasUserMedia() {
 //                     console.log("Unknown source found:", source);
 //                 }
 //             }
-var constraints = {
-    video: {
-        mandatory: {
-            minWidth: 640,
-            minHeight: 480
-        }
-    },
-    audio: true
-};
 if (hasUserMedia()) {
     navigator.getUserMedia = navigator.getUserMedia ||
         navigator.webkitGetUserMedia || navigator.mozGetUserMedia ||
         navigator.msGetUserMedia;
-    navigator.getUserMedia(constraints, function(stream) {
-        var video = document.querySelector('video');
+    var video = document.querySelector('video'),
+        canvas = document.querySelector('canvas'),
+        streaming = false;
+    navigator.getUserMedia({
+        video: true,
+        audio: false
+    }, function(stream) {
         video.src = window.URL.createObjectURL(stream);
-    }, function(err) { console.log("Raised an error when capturing:", error); });
+        streaming = true;
+    }, function(error) {
+        console.log("Raised an error when capturing:", error);
+    });
+    document.querySelector('#capture').addEventListener('click',
+        function(event) {
+            if (streaming) {
+                canvas.width = video.clientWidth;
+                canvas.height = video.clientHeight;
+                var context = canvas.getContext('2d');
+                context.drawImage(video, 0, 0);
+            }
+        });
 } else {
     alert("Sorry, your browser does not support getUserMedia.");
 }
