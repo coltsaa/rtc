833a264acc3c26ded48a9fe130b21006525a1ce2
RYO <ostantares@gmail.com>
2018-05-08 16:42:22 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,28 @@
 function hasUserMedia() {
-	return !!(navigator.getUserMedia || navigator.webkitGetUserMedia
-|| navigator.mozGetUserMedia || navigator.msGetUserMedia);
+    return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia || navigator.msGetUserMedia);
 }
 if (hasUserMedia()) {
-	navigator.getUserMedia = navigator.getUserMedia ||
-	navigator.webkitGetUserMedia || navigator.mozGetUserMedia ||
-	navigator.msGetUserMedia;
-	navigator.getUserMedia({ video: true, audio: true }, function
-(stream) {
-	var video = document.querySelector('video');
-	video.src = window.URL.createObjectURL(stream);
-}, function (err) {});
-} 
-else {
-	alert("Sorry, your browser does not support getUserMedia.");
+    navigator.getUserMedia = navigator.getUserMedia ||
+        navigator.webkitGetUserMedia || navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia;
+    navigator.getUserMedia({
+        video: {
+            mandatory: {
+                minAspectRatio: 1.777,
+                maxAspectRatio: 1.778
+            },
+            optional: [{
+                maxWidth: 640
+            }, {
+                maxHeigth: 480
+            }]
+        },
+        audio: true
+    }, function(stream) {
+        var video = document.querySelector('video');
+        video.src = window.URL.createObjectURL(stream);
+    }, function(err) { console.log("Raised an error when capturing:", error); });
+} else {
+    alert("Sorry, your browser does not support getUserMedia.");
 }
